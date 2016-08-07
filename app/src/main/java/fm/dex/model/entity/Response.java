e7c2b62622538b3f06febe286eac9cef76d6779b
hotchemi <hot.chemistry21@gmail.com>
2016-08-08 01:53:15 +0900

    Add url.

--- app/src/main/java/fm/dex/model/entity/Response.java
+++ app/src/main/java/fm/dex/model/entity/Response.java
@@ -2,6 +2,9 @@ package fm.dex.model.entity;
 
 import java.util.List;
 
+/**
+ * https://raw.githubusercontent.com/dexfm/dexfm.github.io/master/api/feeds.json
+ */
 public final class Response {
 
     private Channel channel;
