2f7e708d8d94729614fc4b48a4e34c0b786e7955
hotchemi <hot.chemistry21@gmail.com>
2016-08-07 18:18:58 +0900

    Add custom app class.

---
+++ app/src/main/java/fm/dex/DexFmApp.java
@@ -0,0 +1,12 @@
+package fm.dex;
+
+import android.app.Application;
+
+public final class DexFmApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
